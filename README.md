bd8171bc9c05ce8a5494d9f73b74c485f7a0b8cd
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-29 05:58:15 +0100

    files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Zure-Nodejs-week-5
---
+++ script.js
@@ -0,0 +1,27 @@
+// console.log("Hello world");
+
+const http = require('http');
+const fs = require('fs');
+const port = 42069;
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data){
+        if (error){
+            res.writeHead(404);
+            res.write("Error: file not found");
+        }else{
+            res.write(data);
+        }
+        res.end();
+    })
+});
+
+server.listen(port, function(error){
+    if (error){
+        console.log("An error occured dipshit", error);
+    }
+    else{
+        console.log(`Server is listening on port ${port}`);
+    }
+})
