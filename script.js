f3944684bfc44151184c4a30ec7eb766ec08ac10
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-29 07:37:42 +0100

    changes

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const http = require('http');
 const fs = require('fs');
 const port = 42069;
 
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data){
-        if (error){
-            res.writeHead(404);
-            res.write("Error: file not found");
-        }else{
-            res.write(data);
-        }
-        res.end();
-    })
-});
+
+// const server = http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     fs.readFile('index.html', function(error, data){
+//         if (error){
+//             res.writeHead(404);
+//             res.write("Error: index.html not found");
+//         }else{
+//             res.write(data);
+//         }
+//         res.end();
+//     })
+// });
 
 server.listen(port, function(error){
     if (error){
