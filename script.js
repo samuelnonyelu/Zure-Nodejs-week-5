5d737cae7b1cdff7cf7b72e68dfdcff635b59fb5
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-30 01:48:51 +0100

    final changes

--- script.js
+++ script.js
@@ -6,27 +6,35 @@ const path = require('path');
 const port = 42069;
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/'){
-        let filePath = path.join(__dirname, 'index.html');
-        fs.readFile(filePath, 'utf8', (error, data) => {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    }
+    let filePath = path.join(__dirname, '.', req.url === '/' ? 'index.html': req.url);
+    let contentType = getContentType(filePath) || 'text/html';
+    let emptyPagePath = path.join(__dirname, '.', '404.html')
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err){
+            if (err.code === 'ENOENT'){
+                fs.readFile(emptyPagePath, 'utf8', (err, content) => {
+                    res.writeHead(200, {'Content-Type': contentType});
+                    res.end(content);
+                });
+            }else{
+                res.writeHead(500);
+                res.end('Server error');
+            }
+        }else if(!err){
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content);
+        }
+    })
 })
 
-// const server = http.createServer(function(req, res){
-//     res.writeHead(200, {'Content-Type': 'text/html'})
-//     fs.readFile('index.html', function(error, data){
-//         if (error){
-//             res.writeHead(404);
-//             res.write("Error: index.html not found");
-//         }else{
-//             res.write(data);
-//         }
-//         res.end();
-//     })
-// });
+const getContentType = (filePath) => {
+    let extname = path.extname(filePath);
+    if (extname === '.js'){
+        return 'text/javascript';
+    }else if (extname === '.css'){
+        return 'text/css';
+    }
+}
 
 server.listen(port, (error) => {
     if (error){
